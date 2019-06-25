1a402dc860d4d921d0f84bbacc85fbe8a92072e9
redwine168 <jredwine1@gmail.com>
2019-06-25 17:29:24 -0400

    testing if button brings user to function

--- public/js/login.js
+++ public/js/login.js
@@ -5,26 +5,18 @@ console.log('running login script')
 const $loginForm = document.querySelector('#login-form')
 const $loginFormUsername = $loginForm.querySelector('username')
 const $loginFormPassword = $loginForm.querySelector('password')
+const $loginFormButton = $loginForm.querySelector('button')
+
 const $createLoginForm = document.querySelector('#create-login-form')
 const $createLoginFormUsername = $createLoginForm.querySelector('username')
 const $createLoginFormPassword = $createLoginForm.querySelector('password')
+const $createLoginFormButton = $createLoginForm.querySelector('button')
 
-$loginForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+$loginForm.addEventListener('submit', checkLogin())
 
-    // Disable form
-    //$messageFormButton.setAttribute('disabled', 'disabled')
 
-    // Retrieve message value of message form
-    const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message, (error) => {
-        // Enable form
 
-        if (error) {
-            return console.log(error)
-        }
-
-        console.log('Message delivered!')
-    })
-})
+function checkLogin() {
+    console.log('in checkLogin function')
+}
