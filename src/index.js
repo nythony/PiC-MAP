34a1ccac589db33d2ed9d1724a831c9dbdfeb2bb
redwine168 <jredwine1@gmail.com>
2019-06-26 12:36:17 -0400

    added missing parenth and bracket

--- src/index.js
+++ src/index.js
@@ -44,139 +44,140 @@ io.on('connection', (socket) => {
         // io.to.emit, socket.broadcast.to.emit
     })
 
-    // Display to everyone
-    socket.on('sendMessage', (message, callback) => {
-        // const filter = new Filter()
+        // Display to everyone
+        socket.on('sendMessage', (message, callback) => {
+            // const filter = new Filter()
 
-        // if (filter.isProfane(message)) {
-        //     return callback('Profanity is not allowed!')
-        // }
+            // if (filter.isProfane(message)) {
+            //     return callback('Profanity is not allowed!')
+            // }
 
-        io.emit('message', generateMessage(message))
->>>>>>> 7dc57d2ce9bf6e925cffc91f2e829d0670018dc9
-        callback()
+            io.emit('message', generateMessage(message))
+    >>>>>>> 7dc57d2ce9bf6e925cffc91f2e829d0670018dc9
+            callback()
+        })
+
+        socket.on('sendLocation', (coords, callback) => {
+            io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+            callback()
+        })
+
+        // When a user disconnects
+        // Disconnect event is built in
+        socket.on('disconnect', () => {
+            io.emit('message', 'A user has left!')
+        })
     })
 
-    socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
-        callback()
+    // console.log("testing console");
+
+    app.get('/', (req,res)=>{
+        //use sendFile since this is a simple html apage
+        //__dirname attaches the file path of the server.js file. 
+        //Since home.thml is in the same directory as server.js, there is nothing else to add to it.
+        res.sendFile(publicDirectoryPath +'/index.html');
+    });
+
+    app.get('/login', (req,res)=>{
+        //use sendFile since this is a simple html apage
+        res.sendFile(views +'userForm.html');
+    });
+
+
+    app.get("/about", function (req, res) {
+        res.sendFile(views + "about.html");
+    });
+
+    app.get("/contact",function(req,res){
+        res.sendFile(views + "contact.html");
+    });
+
+    app.get("/userform",function(req,res){
+        res.sendFile(views + "userForm.html");
+    });
+
+    app.get("/taskform",function(req,res){
+        res.sendFile(views + "taskForm.html");
+    });
+
+    app.get("/chatapp",function(req,res){
+        res.sendFile(views + "chatApp.html")
     })
 
-    // When a user disconnects
-    // Disconnect event is built in
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+    app.get("/loginpage",function(req,res){
+        res.sendFile(views + "loginPage.html")
+    })
+
+    app.get("/chatSignIn",function(req,res){
+        res.sendFile(views + "chatSignIn.html")
+    })
+
+    app.get("/chatApp.html",function(req,res){
+        res.sendFile(views + "chatApp.html")
+    })
+
+    app.post("/contact-submitted", function(req,res){
+        var cname = req.body.name;
+        var cemail = req.body.email;
+        var cmessage = req.body.message;
+        var chuman = req.body.human;
+
+        console.log(cname);
+        console.log(cemail);
+        console.log(cmessage);
+        console.log(chuman);
+    });
+
+    app.post("/userform-submitted", function(req,res){
+        var uname = req.body.name;
+        var uemail = req.body.email;
+        var umessage = req.body.message;
+        var uhuman = req.body.human;
+
+        console.log(uname);
+        console.log(uemail);
+        console.log(umessage);
+        console.log(uhuman);
+    });
+
+    app.post("/taskform-submitted", function(req,res){
+        var task= req.body.task;
+        var taskOwner = req.body.taskOwner;
+        var status = req.body.status;
+
+        console.log(task);
+        console.log(taskOwner);
+        console.log(status);
+    });
+
+
+
+
+
+    // const { Client } = require('pg');
+
+    // const client = new Client({
+    //   connectionString: process.env.DATABASE_URL,
+    //   ssl: true,
+    // });
+
+    // client.connect();
+
+    // client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    //   if (err) throw err;
+    //   for (let row of res.rows) {
+    //     console.log(JSON.stringify(row));
+    //   }
+    //   client.end();
+    // });
+
+
+
+
+
+
+    //This server is running through the port 3000
+    server.listen(port,()=>{
+        console.log(`Server is up on port:${port}`);
     })
 })
-
-// console.log("testing console");
-
-app.get('/', (req,res)=>{
-    //use sendFile since this is a simple html apage
-    //__dirname attaches the file path of the server.js file. 
-    //Since home.thml is in the same directory as server.js, there is nothing else to add to it.
-    res.sendFile(publicDirectoryPath +'/index.html');
-});
-
-app.get('/login', (req,res)=>{
-    //use sendFile since this is a simple html apage
-    res.sendFile(views +'userForm.html');
-});
-
-
-app.get("/about", function (req, res) {
-    res.sendFile(views + "about.html");
-});
-
-app.get("/contact",function(req,res){
-  res.sendFile(views + "contact.html");
-});
-
-app.get("/userform",function(req,res){
-  res.sendFile(views + "userForm.html");
-});
-
-app.get("/taskform",function(req,res){
-  res.sendFile(views + "taskForm.html");
-});
-
-app.get("/chatapp",function(req,res){
-	res.sendFile(views + "chatApp.html")
-})
-
-app.get("/loginpage",function(req,res){
-    res.sendFile(views + "loginPage.html")
-})
-
-app.get("/chatSignIn",function(req,res){
-    res.sendFile(views + "chatSignIn.html")
-})
-
-app.get("/chatApp.html",function(req,res){
-    res.sendFile(views + "chatApp.html")
-})
-
-app.post("/contact-submitted", function(req,res){
-	var cname = req.body.name;
-	var cemail = req.body.email;
-	var cmessage = req.body.message;
-	var chuman = req.body.human;
-
-	console.log(cname);
-	console.log(cemail);
-	console.log(cmessage);
-	console.log(chuman);
-});
-
-app.post("/userform-submitted", function(req,res){
-	var uname = req.body.name;
-	var uemail = req.body.email;
-	var umessage = req.body.message;
-	var uhuman = req.body.human;
-
-	console.log(uname);
-	console.log(uemail);
-	console.log(umessage);
-	console.log(uhuman);
-});
-
-app.post("/taskform-submitted", function(req,res){
-	var task= req.body.task;
-	var taskOwner = req.body.taskOwner;
-	var status = req.body.status;
-
-	console.log(task);
-	console.log(taskOwner);
-	console.log(status);
-});
-
-
-
-
-
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
-
-// client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
-
-
-
-
-
-
-//This server is running through the port 3000
-server.listen(port,()=>{
-    console.log(`Server is up on port:${port}`);
-});
