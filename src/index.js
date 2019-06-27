33b29ebceaa6b44d834314fa8e613885d99f966e
Alina Chaiyasarikul <alinac@bu.edu>
2019-06-26 22:11:40 -0400

    Replacing DATABASE_URL with username/password info

--- src/index.js
+++ src/index.js
@@ -169,7 +169,7 @@ app.post("/taskform-submitted", function(req,res){
  const { Client } = require('pg');
 
  const client = new Client({
-   connectionString: process.env."postgres://yyuppeulmuhcob:205438d2d30f5107605d7fa1c5d8cf4d667eaf0cb2b1608bf01cd4bb77f7bca5@ec2-54-221-212-126.compute-1.amazonaws.com:5432/deku7qrk30lh0",
+   connectionString: process.env."postgres://*picmapmanager*:*MET-Agile3*@*ec2-54-221-212-126.compute-1.amazonaws.com*:*5432*/*deku7qrk30lh0*",
    ssl: true,
  });
 
