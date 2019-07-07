019086947ad7734f7b4e1a3db3224d5c2c888e66
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-07 00:36:48 -0400

    js will take userid from html?

--- public/js/UserHomePage.js
+++ public/js/UserHomePage.js
@@ -1,3 +1,3 @@
-var userid = JSON.stringify(user.userid)
-var projectid = JSON.stringify(user.projectid)
-var taskid = JSON.stringify(user.taskid)
+console.log(userid)
+console.log(projectid)
+console.log(taskid)
