3e1368d2555fb1b475494224d3c839842844bd6a
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-06 20:37:07 -0400

    moving stuff to userhomepage.js

---
+++ public/js/UserHomePage.js
@@ -0,0 +1,7 @@
+// var username = <%- JSON.stringify(user.username) %>
+// var project = <%- JSON.stringify(user.project) %>
+// var task = <%- JSON.stringify(user.task) %>
+
+var username = JSON.stringify(user.username)
+var project = JSON.stringify(user.project)
+var task = JSON.stringify(user.task)
