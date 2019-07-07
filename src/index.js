5a8a50828198bbfad56c76e3936e63e97c72d44b
Alina Chaiyasarikul <alinac@bu.edu>
2019-07-07 17:06:20 -0400

    Getting null instead of values, trying different syntax

--- src/index.js
+++ src/index.js
@@ -239,7 +239,7 @@ app.post("/loginPage/submit", function (req, res) {
                         client.query('SELECT "Project_ID" FROM "User" as Ur RIGHT JOIN "AttachUserP" AS Ap ON Ap."User_ID" = Ur."User_ID" WHERE Ur."UserName" = \'' + username + '\';', (error2, results2) => {
                             if (error2) throw error2 //Should never happen, if anything it returns and stores null
                             var storage = []
-                            for (var obj in results2.rows){
+                            for (let obj of results2.rows){
                                 storage.push(obj["Project_ID"])
                             }
                             res.cookie("userInfo",{name:username, pass:password, projects: storage});
