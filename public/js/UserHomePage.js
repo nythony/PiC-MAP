8edd2c329d4df4140dcfff4fc0c44530277dc38b
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-06 20:50:48 -0400

    we must pass userId

--- public/js/UserHomePage.js
+++ public/js/UserHomePage.js
@@ -1,7 +1,9 @@
-// var username = <%- JSON.stringify(user.username) %>
-// var project = <%- JSON.stringify(user.project) %>
-// var task = <%- JSON.stringify(user.task) %>
-
 var username = JSON.stringify(user.username)
 var project = JSON.stringify(user.project)
 var task = JSON.stringify(user.task)
+
+// client.query('SELECT "ProjectName" FROM "Project";', (error, results) => {
+//     for (let row of results.row) {
+//         if (row[""])
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ app.set('views', path.join(__dirname, '../public/views'))
 
 // Trying to mask some user authentication
 class Passer {
-    constructor(username, project, task){
-        this.username = username
+    constructor(userid, project, task){
+        this.username = userid
         this.project = project
         this.task = task
     }
@@ -220,7 +220,9 @@ app.post("/loginPage/submit", function (req, res) {
                     if (error1) throw error1
                     if (results1["rows"][0]["Password"] == password) {
                         console.log('test1')
-                        AuthUser = new Passer(username, null, null)
+                        client.query('SELECT "User_ID" FROM "USER" WHERE "UserName" = \'' + username + '\';', (error1, results2))
+                        var userid = results2["rows"][0]["User_ID"]
+                        AuthUser = new Passer(userid, null, null)
                         toRedirect = '/UserHomePage/' + AuthUser
                         console.log(toRedirect)
                     }
