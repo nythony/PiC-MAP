2812975748316600458f86542e629a7485d2d047
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-06 22:05:51 -0400

    really?

--- public/js/UserHomePage.js
+++ public/js/UserHomePage.js
@@ -1,7 +1,3 @@
-// var username = <%- JSON.stringify(user.username) %>
-// var project = <%- JSON.stringify(user.project) %>
-// var task = <%- JSON.stringify(user.task) %>
-
 var username = JSON.stringify(user.username)
 var project = JSON.stringify(user.project)
 var task = JSON.stringify(user.task)
