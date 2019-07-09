7a8a20f8870fff84af64204f71be0a3ba512ccfc
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-08 22:03:56 -0400

    we have to include userid because of db constraint

--- src/index.js
+++ src/index.js
@@ -79,8 +79,8 @@ io.on('connection', (socket) => {
     // Display to everyone
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id)
-        const text = 'INSERT INTO "ChatMessage"( "Message" ) VALUES($1) RETURNING *'
-        const values = [message.text]
+        const text = 'INSERT INTO "ChatMessage"( "User_ID", "Message" ) VALUES($1, $2) RETURNING *'
+        const values = [6, message.text]
         client.query(text, values, (err, res) => {
             if (err) {
                 console.log(err.stack)
