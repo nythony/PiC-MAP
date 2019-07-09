9c51bd70d1dd253278552e18962b16bd3da6edd5
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-08 22:06:41 -0400

    have to include userid, chatroomid

--- src/index.js
+++ src/index.js
@@ -79,8 +79,8 @@ io.on('connection', (socket) => {
     // Display to everyone
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id)
-        const text = 'INSERT INTO "ChatMessage"( "User_ID", "Message" ) VALUES($1, $2) RETURNING *'
-        const values = [6, message.text]
+        const text = 'INSERT INTO "ChatMessage"( "User_ID", "ChatRoom_ID", "Message" ) VALUES($1, $2, $3) RETURNING *'
+        const values = [6, 1, message.text]
         client.query(text, values, (err, res) => {
             if (err) {
                 console.log(err.stack)
