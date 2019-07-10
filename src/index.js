4758b4a205f1b94940a18d0a644b244427806989
redwine168 <jredwine1@gmail.com>
2019-07-10 19:22:10 -0400

    fixed a sneaky typo in cookie copy

--- src/index.js
+++ src/index.js
@@ -268,7 +268,7 @@ app.post("/UserHomePage/joinProject", function (req, res) {
         const attachText = 'INSERT INTO "AttachUserP"("User_ID", "Project_ID") VALUES($1,$2) RETURNING *'
         client.query(attachText, attachValues, (err2, res2) => {
             if (err2) {console.log(err2.stack)}
-            res.cookie("userInfo", req.cookies.userinfo) // copy req cookie into res cookie
+            res.cookie("userInfo", req.cookies.userInfo) // copy req cookie into res cookie
             res.cookies.userInfo.projects.push(projectid) // add new project to res cookie
             res.redirect('/UserHomePage');
         })
