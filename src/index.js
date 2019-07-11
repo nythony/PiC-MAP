4a6b736a31f6de4e6659adf7f006124654e3f557
redwine168 <jredwine1@gmail.com>
2019-07-11 00:17:30 -0400

    updated query for IDstring

--- src/index.js
+++ src/index.js
@@ -292,8 +292,7 @@ app.post("/UserHomePage/viewProject", function (req, res) {
                 }
             }
             IDstring += ')'
-            console.log(IDstring)
-            client.query('SELECT "UserName" FROM "User" WHERE "User_ID" IN ('+JSON.stringify(teamIDs)+');', (err2, teamnameresult) => {
+            client.query('SELECT "UserName" FROM "User" WHERE "User_ID" IN '+IDstring+');', (err2, teamnameresult) => {
                 var teamNames = []
                 for (let teammate of teamnameresult["rows"]){
                     teamNames.push(teammate["UserName"])
