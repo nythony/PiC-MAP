ae4ad39d390942c4221d0db9c236fdb60bbeedd0
redwine168 <jredwine1@gmail.com>
2019-07-11 00:12:02 -0400

    fixed final index where comma is added

--- src/index.js
+++ src/index.js
@@ -284,7 +284,7 @@ app.post("/UserHomePage/viewProject", function (req, res) {
             }
             var IDstring = '('
             var i;
-            for (i = 0; i < teamIDs.length-1; i++) {
+            for (i = 0; i < teamIDs.length-2; i++) {
                 IDstring += (teamIDs[i]).toString()
                 IDstring += ','
             }
