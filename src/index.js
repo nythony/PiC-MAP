4671c25d97468ac5177a74edaba5a1350889d141
redwine168 <jredwine1@gmail.com>
2019-07-12 15:23:38 -0400

    trying login functionality from loginTools.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const requirement = require('./requirementForm.js')
 const task = require('./taskForm.js')
 const taskTool = require('./taskToolForm.js')
 const issue = require('./issueForm.js')
+const login = require('./loginTools.js')
 
 //Connecting to cloud based database:
 const client = new Client({
@@ -350,45 +351,7 @@ app.post("/loginPage/submit", function (req, res) {
     var username = req.body.username
     var password = req.body.password
     var toRedirect = '/failedLoginPage'
-    var loginMatch = client.query('SELECT user_pass_match(\''+username+'\',\''+password+'\');')
-    console.log('login return: ', loginMatch)
-    client.query('SELECT "Project_ID" FROM "User" as Ur RIGHT JOIN "AttachUserP" AS Ap ON Ap."User_ID" = Ur."User_ID" WHERE Ur."UserName" = \'' + username + '\';', (error2, results2) => {
-        if (error2) throw error2 //Should never happen, if anything it returns and stores null
-        var storage = []
-        for (let obj of results2.rows) {
-            storage.push(obj["Project_ID"])
-        }
-        client.query('SELECT "User_ID" FROM "User" WHERE "UserName" = \'' + username + '\';', (error1, useridresult) => {
-            client.query('SELECT "Project_ID" FROM "User" as Ur RIGHT JOIN "AttachUserP" AS Ap ON Ap."User_ID" = Ur."User_ID" WHERE Ur."UserName" = \'' + username + '\';', (error2, results2) => {
-                if (error2) throw error2 //Should never happen, if anything it returns and stores null
-                var storage = []
-                for (let obj of results2.rows){
-                    storage.push(obj["Project_ID"])
-                }
-                var IDstring = '('
-                var i;
-                for (i = 0; i < storage.length; i++) { // put in the form of (id1, id2, id3, ...), as this is needed for the IN query
-                    IDstring += (storage[i]).toString()
-                    if (i != storage.length-1) {
-                        IDstring += ','
-                    }
-                }
-                IDstring += ')'
-                client.query('SELECT "ProjectName", "ProjectDesc" FROM "Project" WHERE "Project_ID" IN '+IDstring+';', (error3, projectnameresult) => {
-                    var pNames = []
-                    var pDescs = []
-                    for (let project of projectnameresult["rows"]){ // get the names of all users whose IDs we have
-                        pNames.push(project["ProjectName"])
-                        pDescs.push(project["ProjectDesc"])
-                    }
-                    res.cookie("userInfo",{name:username, userid: useridresult["rows"][0]["User_ID"], pass:password, projects: storage, projectNames: pNames, 
-                        projectDescs: pDescs, chatname: "TestingChatroom", chatroomid: 1, currProjectID: 0, currProjectName: null, taskTools: []});
-                    toRedirect = '/UserHomePage/'
-                    res.redirect(toRedirect)
-                })
-            })
-        })
-    })                
+    login.verifyCredentials(req, res, username, password)               
 })
 
 app.post("/failedLoginPage/submit", function (req, res) {
---
+++ src/loginTools.js
@@ -0,0 +1,68 @@
+// Login Tools
+
+/*
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const { Client } = require('pg');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    //connectionString: "postgres://yyuppeulmuhcob:205438d2d30f5107605d7fa1c5d8cf4d667eaf0cb2b1608bf01cd4bb77f7bca5@ec2-54-221-212-126.compute-1.amazonaws.com:5432/deku7qrk30lh0",
+    ssl: true,
+});
+*/
+
+client.connect();
+
+const verifyCredentials(req, res, username, password) => {
+    var loginMatch = client.query('SELECT user_pass_match(\''+username+'\',\''+password+'\');').then()
+    console.log('login return: ', loginMatch)
+    client.query('SELECT "Project_ID" FROM "User" as Ur RIGHT JOIN "AttachUserP" AS Ap ON Ap."User_ID" = Ur."User_ID" WHERE Ur."UserName" = \'' + username + '\';', (error2, results2) => {
+        if (error2) throw error2 //Should never happen, if anything it returns and stores null
+        var storage = []
+        for (let obj of results2.rows) {
+            storage.push(obj["Project_ID"])
+        }
+        client.query('SELECT "User_ID" FROM "User" WHERE "UserName" = \'' + username + '\';', (error1, useridresult) => {
+            client.query('SELECT "Project_ID" FROM "User" as Ur RIGHT JOIN "AttachUserP" AS Ap ON Ap."User_ID" = Ur."User_ID" WHERE Ur."UserName" = \'' + username + '\';', (error2, results2) => {
+                if (error2) throw error2 //Should never happen, if anything it returns and stores null
+                var storage = []
+                for (let obj of results2.rows){
+                    storage.push(obj["Project_ID"])
+                }
+                var IDstring = '('
+                var i;
+                for (i = 0; i < storage.length; i++) { // put in the form of (id1, id2, id3, ...), as this is needed for the IN query
+                    IDstring += (storage[i]).toString()
+                    if (i != storage.length-1) {
+                        IDstring += ','
+                    }
+                }
+                IDstring += ')'
+                client.query('SELECT "ProjectName", "ProjectDesc" FROM "Project" WHERE "Project_ID" IN '+IDstring+';', (error3, projectnameresult) => {
+                    var pNames = []
+                    var pDescs = []
+                    for (let project of projectnameresult["rows"]){ // get the names of all users whose IDs we have
+                        pNames.push(project["ProjectName"])
+                        pDescs.push(project["ProjectDesc"])
+                    }
+                    res.cookie("userInfo",{name:username, userid: useridresult["rows"][0]["User_ID"], pass:password, projects: storage, projectNames: pNames, 
+                        projectDescs: pDescs, chatname: "TestingChatroom", chatroomid: 1, currProjectID: 0, currProjectName: null, taskTools: []});
+                    toRedirect = '/UserHomePage/'
+                    res.redirect(toRedirect)
+                })
+            })
+        })
+    })
+
+
+
+
+
+}
+
+
+module.exports = {
+    verifyCredentials,
+}
