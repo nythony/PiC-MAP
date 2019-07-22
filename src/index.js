e47e361865cedf67a67510fd1e944aa31ed9a263
Alina Chaiyasarikul <alinac@bu.edu>
2019-07-22 19:23:35 -0400

    Querying projects from URL

--- src/index.js
+++ src/index.js
@@ -190,8 +190,8 @@ io.on('connection', (socket) => {
         var username = userProj.username;
         var list = []
         console.log("USERNAME: ", username)
-        const text = 'SELECT Pa."Project_ID", Pa."ProjectName", Pa."ProjectDesc" FROM "Project" Pa JOIN "AttachUserP" Ap ON Ap."Project_ID" = Pa."Project_ID" JOIN "User" Up ON Up."User_ID" = Ap."User_ID" WHERE "UserName" = \''+username+'\ ORDER BY "StartDate"'
-        
+        //const text = 'SELECT Pa."Project_ID", Pa."ProjectName", Pa."ProjectDesc" FROM "Project" Pa JOIN "AttachUserP" Ap ON Ap."Project_ID" = Pa."Project_ID" JOIN "User" Up ON Up."User_ID" = Ap."User_ID" WHERE "UserName" = '\''+username+'\' ORDER BY "StartDate"'
+        const text = 'SELECT "User_ID" FROM "User" WHERE "UserName" = \'' + username + '\';'
         console.log("PROJECT LIST")
         client.query(text, (err, results) => { 
             for (let obj of results.rows){
