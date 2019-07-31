0210000b9bbc459e622aa185b8cdb490f5651ed1
redwine168 <jredwine1@gmail.com>
2019-07-31 17:01:38 -0400

    gave each nav button a template

--- public/js/ProjectHomePage.js
+++ public/js/ProjectHomePage.js
@@ -5,6 +5,10 @@ const socket = io()
 // Elements
 const $header = document.querySelector('#header')
 const $taskTool = document.querySelector('#taskTool')
+const $reqButton = document.querySelector('#reqButton')
+const $issuesButton = document.querySelector('#$issuesButton')
+const $chatappButton = document.querySelector('#$chatappButton')
+
 const $createTaskToolForm = document.querySelector('#createTaskToolForm')
 const $editTaskToolForm = document.querySelector('#editTaskToolForm')
 const $deleteTaskToolForm = document.querySelector('#deleteTaskToolForm')
@@ -12,6 +16,9 @@ const $deleteTaskToolForm = document.querySelector('#deleteTaskToolForm')
 // Templates
 const headerTemplate = document.querySelector('#header-template').innerHTML
 const taskToolTemplate = document.querySelector('#tasktool-template').innerHTML
+const requirementsButtonTemplate = document.querySelector('#requirementsButton-template').innerHTML
+const issuesButtonTemplate = document.querySelector('#issuesButton-template').innerHTML
+const chatappButtonTemplate = document.querySelector('#chatappButton-template').innerHTML
 
 
 
@@ -26,6 +33,34 @@ const headerhtml = Mustache.render(headerTemplate, {
     projectName: projectNameVP
 })
 document.querySelector('#header').innerHTML = headerhtml
+
+
+// Fill requirements button with necessary navigation info
+const rButton = Mustache.render(requirementsButtonTemplate, {
+    username: usernameVP,
+    userid: useridVP,
+    ProjectName: projectNameVP,
+    Project_ID: projectidVP
+})
+$reqButton.innerHTML = rButton
+
+// Fill issues button with necessary navigation info
+const iButton = Mustache.render(issuesButtonTemplate, {
+    username: usernameVP,
+    userid: useridVP,
+    ProjectName: projectNameVP,
+    Project_ID: projectidVP
+})
+$issuesButton.innerHTML = iButton
+
+// Fill chatapp button with necessary navigation info
+const cButton = Mustache.render(chatappButtonTemplate, {
+    username: usernameVP,
+    userid: useridVP,
+    ProjectName: projectNameVP,
+    Project_ID: projectidVP
+})
+$chatappButton.innerHTML = cButton
 
 
 
